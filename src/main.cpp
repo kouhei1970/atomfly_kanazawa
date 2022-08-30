6f5cb3c336fdbc4a51e3243ce16608630be65a30
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2022-08-30 21:04:06 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <M5Atom.h>
+#include "rc.hpp"
 #include "control.hpp"
 
 //VL53L0X_ADDRESS           0x29
@@ -7,23 +8,18 @@
 //BMP280_ADDRESS            0x76
 
 
-void setup() {
-  unsigned long start_time, end_time;  
-  start_time = micros();
-
+void setup() {  
   M5.begin(true, true, true);
-  Serial.println();
-  Serial.println(start_time);
   init_atomfly();
-  end_time = micros();
-  Serial.println(end_time);
-  Serial.println(end_time - start_time);
+  delay(1000);
 }
 
 //float Pitch,Roll,Yaw;
 
 void loop() {
-  atomfly_main();
-  M5.dis.drawpix(0, 0xff00ff);
+  loop_400Hz();
+  //rc_demo();
+  //atomfly_main();
+  //M5.dis.drawpix(0, 0xff00ff);
   //delay(33);
 }
